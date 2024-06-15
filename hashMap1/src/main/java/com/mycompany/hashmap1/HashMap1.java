604fb4cc73577c96f9c68c546509b82cd9b151fe
Leyder2023 <151747360+Leyder2023@users.noreply.github.com>
2024-06-15 00:10:07 -0500

    Add files via upload

---
+++ hashMap1/src/main/java/com/mycompany/hashmap1/HashMap1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hashmap1;
+
+/**
+ *
+ * @author john
+ */
+public class HashMap1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ hashMap3/src/main/java/com/mycompany/hashmap3/HashMap3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hashmap3;
+
+/**
+ *
+ * @author john
+ */
+public class HashMap3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
